4c4b50ed34e76dd3afb2aaf5d7cee4f43f0ab494
Yurii Matora <yura.matora11@gmail.com>
2021-06-12 23:58:02 +0300

    Initial commit

---
+++ src/CommonTasks.py
@@ -0,0 +1,14 @@
+def simpleSum(a: int, b: int):
+    return a + b
+
+
+def simpleProduct(a: int, b: int):
+    return a * b
+
+
+def simpleDivide(a: int, b: int):
+    return a // b
+
+
+def simplePow(a: int, b: int):
+    return pow(a, b)
